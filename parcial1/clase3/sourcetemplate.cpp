bf36fdf41b6fb04a99cf196e86f301b4e47c14ea
Ana Laura Tagle Nardelli Pinto <90301249+tuseniorjsus@users.noreply.github.com>
2023-01-16 12:12:32 -0600

    Templates de uso c++

---
+++ parcial1/clase3/sourcetemplate.cpp
@@ -0,0 +1,41 @@
+#include "ColaL.h"
+void menu()
+{
+	cout << "FRASES" << endl; 
+	cout<<"1.Agregar frase" << endl; 
+	cout << "2.Extraer frase" << endl; 
+	cout << "3.Imprimir frases" << endl; 
+	cout << "4.Salir" << endl; 
+}
+int main()
+{
+	ColaL cola1; 
+	
+	do {
+		int resp; 
+		string frase; 
+		menu(); 
+		cout << "Ingresa una opción: " << endl; 
+		cin >> resp; 
+		switch (resp)
+		{case 1 : 
+			cout << "Ingrese una frase: " << endl; 
+			getline(cin, frase); 
+			cola1.Queue(frase); 
+			break; 
+		case 2: 
+			cola1.Dequeue(); 
+			break; 
+		case 3:
+			cola1.Show(); 
+			break; 
+		case 4: 
+			cout << "Hasta Luego" << endl; 
+			break; 
+
+		default:
+			cout << "Ingrese una opcion valida: " << endl; 
+			break;
+		}
+	}while(resp!=5)
+}
