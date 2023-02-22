a781593e648e11e012a5380e004275f8a3638ba5
Ana Laura Tagle Nardelli Pinto <90301249+tuseniorjsus@users.noreply.github.com>
2023-02-22 12:43:13 -0600

    newc
    changes

--- parcial1/clase11/c++_anahuac/c++_anahuac/arregloscpp.cpp
+++ parcial1/clase11/c++_anahuac/c++_anahuac/arregloscpp.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std; 
 int main()
-{
+{//a 
+	int quantity = 5; 
+	//int* vale; 
+	vale = new string[quantity]; 
 	int mynewarr[10] = { 10,45,20,35,50,60,8,99,67,77 }; 
 	int suma = 0, max = 0, newindex; 
 	for (int i = 0; i < 10; i++)
 	{
+		//b
 		suma = suma + mynewarr[i]; 
 		cout << mynewarr[i] << ","; 
 	}
 	cout << endl << "La suma de los elemntos es de: " << suma << endl; 
 	cout << "Arreglo Inverso: " << endl; 
-	for (int j = 9; j > 0; j--)
-	{
+	for (int j = 9; j >= 0; j--)
+	{//c
 		suma = suma + mynewarr[j];
 		cout << mynewarr[j] << ",";
 	}
+	//d
 	cout << endl<<"El valor mas alto: " << endl; 
 	for (int k = 0; k < 10; k++)
 	{
